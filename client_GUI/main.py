6bc187ed58b99904c949a17e90a7c3f39aa9c439
npc203 <npc203@users.noreply.github.com>
2020-09-13 02:32:12 +0530

    refactored routes and partially added login

--- client_GUI/main.py
+++ client_GUI/main.py
@@ -1,36 +1,45 @@
 from flask import Flask,render_template,flash,redirect
 from flaskwebgui import FlaskUI 
-from form_parts import item
+from form_parts import item,LoginForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'make this a secret later lol'
-ui = FlaskUI(app)
+#ui = FlaskUI(app)
 
 
 ui = FlaskUI(app)
 
-@app.route("/")
+#where the user lands if logged in successfully
+@app.route('/index', methods=['GET', 'POST'])
 def index():
-  return render_template('base.html')
+    return render_template('base.html')
 
-@app.route('/new_item')
+#Login for the user
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        # Get the shop name from the DB based on the username and pass
+        return render_template('base.html',data={"shop_name":"Shop name from DB here"})
+    return render_template('login.html', title='Sign In', form=form)
+
+#Form for the user to add a new item
+@app.route('/new_item',methods=['GET', 'POST'])
 def add_item():
     form = item()
-    return render_template('add_item.html', title='It is done?', form=form)
-@app.route('/success')
-def success():
-    return render_template('success.html', title='Successfully added')
-
-@app.route('/new_item', methods=['GET', 'POST'])
-def login():
-    form = item()
-    print(form.validate_on_submit())
     if form.validate_on_submit():
         datas=[form.name.data,form.description.data,form.price.data]
         [flash(i) for i in datas]
         return redirect('/success')
-    return render_template('add_item.html', title='New item', form=form)
+    else:
+        return render_template('add_item.html', title='Error', form=form,data="Something went Wrong")
+    return render_template('add_item.html', title='Adding a new item', form=form)
 
+#page where the user lands if item added successfully
+@app.route('/success')
+def success():
+    return render_template('success.html', title='Successfully added')
 
-#app.run(debug=True)
-ui.run()
+if __name__ == "__main__":
+    app.run(debug=True)
+    #ui.run()
