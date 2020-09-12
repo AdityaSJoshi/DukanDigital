4ac164de56c5d542882bcdfe00579ecfe0b3c092
npc203 <npc203@users.noreply.github.com>
2020-09-12 23:04:06 +0530

    added flash cards maybe

--- client_GUI/add_product.py
+++ client_GUI/add_product.py
@@ -1,9 +1,10 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, IntegerField
 from wtforms.validators import DataRequired
 
 class item(FlaskForm):
-    username = StringField('Title', validators=[DataRequired()])
+    name = StringField('Name', validators=[DataRequired()])
     description = StringField('Description', validators=[DataRequired()])
-    remember_me = BooleanField('Some check mark kinda')
+    price = IntegerField('Price',validators=[DataRequired()])
+    #remember_me = BooleanField('Some check mark kinda')
     submit = SubmitField('Add Item')
--- client_GUI/main.py
+++ client_GUI/main.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,flash,redirect
 from flaskwebgui import FlaskUI 
 from add_product import item
 
@@ -12,12 +12,19 @@ app.config['SECRET_KEY'] = 'make this a secret later lol'
 def index():
   return render_template('base.html')
 
-
-
 @app.route('/new_item')
 def add_item():
     form = item()
     return render_template('add_item.html', title='Sign In', form=form)
+
+@app.route('/new_item', methods=['GET', 'POST'])
+def login():
+    form = item()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.name.data, form.remember_me.data))
+        return redirect('/index')
+    return render_template('add_item.html', title='New item', form=form)
 
 
 app.run(debug=True)
