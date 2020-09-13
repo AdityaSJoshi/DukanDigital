bc1e853a990da0149930af55a0d472818eff4f53
npc203 <npc203@users.noreply.github.com>
2020-09-13 13:02:05 +0530

    added register

--- client_GUI/main.py
+++ client_GUI/main.py
@@ -1,7 +1,7 @@
 from flask import Flask,render_template,flash,redirect
 from flaskwebgui import FlaskUI 
 from database.models import USER
-from form_parts import item
+from form_parts import item,LoginForm,Register
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'make this a secret later lol'
@@ -9,10 +9,6 @@ app.config['SECRET_KEY'] = 'make this a secret later lol'
 
 
 ui = FlaskUI(app)
-
-@app.route("/")
-def index():
-    return render_template('base.html')
 
 #Login for the user
 @app.route('/', methods=['GET', 'POST'])
@@ -22,6 +18,18 @@ def login():
         # Get the shop name from the DB based on the username and pass
         return render_template('base.html',data={"shop_name":"Shop name from DB here"})
     return render_template('login.html', title='Sign In', form=form)
+
+#register for user
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = Register()
+    if form.validate_on_submit():
+        # make sure to check for conflicts with existing shop names? nahh
+        datas=[form.Name.data,form.Email.data,form.Password.data,form.ShopName.data,form.ShopDescription.data]
+        return render_template('register.html')
+    #else:
+    #    return render_template('register.html', title='Error', form=form,data="Something went Wrong")
+    return render_template('register.html', title='Sign up', form=form)
 
 #Form for the user to add a new item
 @app.route('/new_item',methods=['GET', 'POST'])
